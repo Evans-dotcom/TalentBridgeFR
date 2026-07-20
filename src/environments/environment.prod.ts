f397c4a255fa507f571e4ae2f3161f3e69dab56c
Evans <evaslangat@gmail.com>
2026-07-20 22:21:45 +0300

    locals

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  //apiUrl: 'https://TalentBridge.onrender.com/api',
-  apiUrl: 'http://localhost:8084/api/v1',
+  apiUrl: 'https://TalentBridge.onrender.com/api',
+  //apiUrl: 'http://localhost:8084/api/v1',
   whatsappNumber: '254719127100',
   cloudinaryCloudName: 'dtpczagws',
   appName: 'TalentBridge'
