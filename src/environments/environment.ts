e781493bd38c5684033848d55fdf1242328fcbc7
Evans <evaslangat@gmail.com>
2026-07-21 11:36:52 +0300

    Env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://talentbridge.onrender.com/api/v1',
+  apiUrl: 'https://takentbridge.onrender.com/api/v1',
   whatsappNumber: '254719127100',
   cloudinaryCloudName: 'dtpczagws',
   appName: 'TalentBridge'
