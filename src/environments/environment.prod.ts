ccafbb67026e5dc6d304ffc8e14c8d8aadc77f57
Evans <evaslangat@gmail.com>
2026-07-21 11:24:47 +0300

    Envi

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://TalentBridge.onrender.com/api',
+  apiUrl: 'https://TalentBridge.onrender.com/api/v1',
   //apiUrl: 'http://localhost:8084/api/v1',
   whatsappNumber: '254719127100',
   cloudinaryCloudName: 'dtpczagws',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://takentbridge.onrender.com/api/v1',
-  //apiUrl: 'http://localhost:8084/api/v1',
+  apiUrl: 'https://talentbridge.onrender.com/api/v1',
   whatsappNumber: '254719127100',
   cloudinaryCloudName: 'dtpczagws',
   appName: 'TalentBridge'
 };
-
